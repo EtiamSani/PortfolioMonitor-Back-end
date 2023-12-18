fb724f7278a51aa46d368c69b435285bb51a57e9
EtiamSani <etiam.sani@laposte.net>
2023-12-18 19:40:17 +0100

    modification of model

---
+++ prisma/migrations/20231218183746_add_column_porfolio_table/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Portfolio" ADD COLUMN     "gainOrLost" INTEGER,
+ADD COLUMN     "liquidity" INTEGER,
+ADD COLUMN     "moneyInput" INTEGER,
+ADD COLUMN     "portfolioValue" INTEGER;
--- src/portfolio/dto/portfolio.dto.ts
+++ src/portfolio/dto/portfolio.dto.ts
@@ -1,8 +1,17 @@
-import {IsNotEmpty, IsString} from 'class-validator';
+import {IsNotEmpty, IsNumber, IsString} from 'class-validator';
 
 export class PortfolioDto {
     @IsString()
     name: string;
+
+    @IsNumber()
+    moneyInput?: number;
+    @IsNumber()
+    liquidity?: number;
+    @IsNumber()
+    gainOrlost?: number;
+    @IsNumber()
+    portfolioValue?: number;
 
     @IsString()
     portfolioOwnerId: string;
