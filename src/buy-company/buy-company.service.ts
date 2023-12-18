141d5bdddfbf4e7389f758803853489fba4cf89c
EtiamSani <etiam.sani@laposte.net>
2023-12-18 17:36:46 +0100

    buy news share logic, it add to existent shares

--- src/buy-company/buy-company.service.ts
+++ src/buy-company/buy-company.service.ts
@@ -7,7 +7,6 @@ export class BuyCompanyService {
     constructor(private prisma: PrismaService){}
     async buyNewShareOfACompany(@Param('companyId') companyId: string, @Body() dto:BuyCompanyDto){
         try {
-            console.log(dto)
             const buyNewShareOfACompany = await this.prisma.buy.create({
                 data: {
                     nature: dto.nature,
@@ -19,10 +18,52 @@ export class BuyCompanyService {
                 },
             }
             )
-            return buyNewShareOfACompany
+            const addedShareId = buyNewShareOfACompany.id
+            this.addNewBoughtShareToCompany(companyId,addedShareId)
+            this.updatePru(companyId, dto.newPru)
+            return addedShareId
             } catch(error) {
                 console.log(error);
             throw new Error('Une erreur est survenue lors de la création de l\'entreprise et de la liaison au portefeuille.');
             }
+    }
+
+    async addNewBoughtShareToCompany(@Param('companyId') companyId: string, addedShareId: string){
+        const findCompany = await this.prisma.company.findUnique({
+            where: {
+                id: companyId
+            }
+        })
+        if (findCompany) {
+            const numberOfStocks = findCompany.numberOfStocks; 
+            const getNumberOfBoughtShares = await this.prisma.buy.findUnique({
+                where: {
+                    id: addedShareId
+                }
+            })
+            const numberOfBoughtShares = getNumberOfBoughtShares.numberOfStocks
+            const addingBuyedStockToExistentStockInCompany = numberOfStocks + numberOfBoughtShares 
+            const updateNumberOfSharesInCompany = await this.prisma.company.update({
+                where: {
+                    id: companyId
+                },
+                data: {
+                    numberOfStocks: addingBuyedStockToExistentStockInCompany
+                }
+            })
+            return updateNumberOfSharesInCompany
+        }
+
+    }
+    async updatePru(@Param('companyId') companyId: string, newPru: number){
+        const findCompany = await this.prisma.company.update({
+            where: {
+                id: companyId
+            },
+            data: {
+                pru: newPru
+            }
+        })
+        return findCompany
     }
 }
