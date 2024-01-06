7f8e82f60079fa24092096f9f50f722a69eaaec8
EtiamSani <etiam.sani@laposte.net>
2024-01-06 01:24:08 +0100

    pagination logic

--- src/owner-analysis/owner-analysis.controller.ts
+++ src/owner-analysis/owner-analysis.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Param, Post, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Query, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Express } from 'express'
 import { OwnerAnalysisService } from './owner-analysis.service';
 import { Response } from 'express';
+import { AnalysisDto } from './dto/analysis.dto';
 
 @Controller('owner-analysis')
 export class OwnerAnalysisController {
@@ -14,10 +15,18 @@ uploadFile(@UploadedFile() file: Express.Multer.File, @Param('ownerId') ownerId:
   return this.ownerAnalysisService.uploadFile(file,ownerId)
 }
 
-@Get('all-pdfs')
-    async getAllPDFs() {
-        return this.ownerAnalysisService.getAllAnalysisPDFs();
+@Get('all-pdfs-total')
+    async getAllPDFsTotal() {
+        return this.ownerAnalysisService.getAllAnalysisPDFsTotal();
     }
+
+@Get('all-pdfs')
+async getAllPDFs(
+  @Query('page') page: number = 1,
+  @Query('pageSize') pageSize: number = 10,
+) {
+  return this.ownerAnalysisService.getAllAnalysisPDFs(page, pageSize);
+}
 
 @Get('download/:pdfId')
     async downloadPDF(@Param('pdfId') pdfId: string, @Res() res: Response) {
@@ -26,4 +35,9 @@ uploadFile(@UploadedFile() file: Express.Multer.File, @Param('ownerId') ownerId:
         res.setHeader('Content-Disposition', 'attachment; filename="downloaded.pdf"');
         res.send(Buffer.from(analysis.file));
     }  
+
+@Patch('analysis-data/:pdfId')
+    async updateAnalysisDate(@Param('pdfId') pdfId: string, @Body() dto:AnalysisDto){
+       return this.ownerAnalysisService.updateAnalysisDate(pdfId, dto)
+    }
 }
--- src/owner-analysis/owner-analysis.service.ts
+++ src/owner-analysis/owner-analysis.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, NotFoundException, Param } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AnalysisDto } from './dto/analysis.dto';
 
 @Injectable()
 export class OwnerAnalysisService {
@@ -21,9 +22,22 @@ export class OwnerAnalysisService {
          
     }
 
-    async getAllAnalysisPDFs() {
+    async getAllAnalysisPDFsTotal() {
         return this.prisma.analysis.findMany();
     }
+
+    async getAllAnalysisPDFs(page: number = 1, pageSize: number = 20) {
+        
+        const offset = (page - 1) * pageSize;
+      
+        const analyses = await this.prisma.analysis.findMany({
+          skip: offset,
+          take: Number(pageSize),
+        });
+      
+        return analyses;
+      }
+      
 
     async downloadPDF(pdfId: string) {
         const analysis = await this.prisma.analysis.findUnique({
@@ -35,5 +49,20 @@ export class OwnerAnalysisService {
         }
 
         return analysis;
+    }
+
+    async updateAnalysisDate(pdfId: string, dto:AnalysisDto) {
+        const updateAnalysisData = await this.prisma.analysis.updateMany({
+            where: { 
+                id: pdfId
+             },
+             data: {
+                fairValue: dto.fairValue, 
+                stockCategory: dto.stockCategory, 
+                peaOrCto: dto.peaOrCto, 
+                entryPoint: dto.entryPoint, 
+             } 
+        });
+        return updateAnalysisData;
     }
 }
